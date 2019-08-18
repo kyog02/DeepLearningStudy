d0dc2bd1aa507f426849717667e28d8c4317b6eb
kyog02 <kyog02.m@gmail.com>
2019-08-19 02:21:18 +0900

    ch06 comp

--- activation_func_layers.py
+++ activation_func_layers.py
@@ -97,3 +97,129 @@ class SoftmaxWithLossLayer:
         batch_size = self.t.shape[0]
         dx = (self.y - self.t) / batch_size
         return dx
+
+# ドロップアウトレイヤ
+# 過学習抑制する手法の一つ
+# ニューロンをランダムに消去しながら学習する手法
+# 訓練時に、隠れ層のニューロンをランダムに選び出し、その選び出したニューロンを消去する。
+# テスト時には、すべてのニューロンの信号を伝達するが、各ニューロンの出力に対して、訓練時に消去した割合を乗算して出力する。
+# 消去する判断のレシオはハイパーパラメータである。
+# 活性化関数レイヤの後に入れる。(最後の出力層の後には不要)
+# メリット：学習時にニューロンをランダムに消去することで、毎回異なるモデルを学習させていると解釈できる。アンサンブル学習と似た手法
+# 補足：バッチ正規化と一緒に使うことは少ないらしい。
+class DropoutLayer:
+    """
+    http://arxiv.org/abs/1207.0580
+    """
+    def __init__(self, dropout_ratio=0.15):
+        self.dropout_ratio = dropout_ratio
+        self.mask = None
+
+    def forward(self, x, train_flg=True):
+        if train_flg:
+            # 訓練時は、xと同じ形状の配列をランダムに生成して、レシオよりも小さかった要素を消去する。
+            self.mask = np.random.rand(*x.shape) > self.dropout_ratio
+            return x * self.mask
+        else:
+            # テスト時は、訓練時に消去した割合を乗算して出力
+            return x * (1.0 - self.dropout_ratio)
+
+    def backward(self, dout):
+        return dout * self.mask
+
+
+
+# バッチ正規化レイヤ(BatchNormalization)
+# 重みの初期値を適切に設定しないと、各層のアクティベーション分布が広がらないという問題に対して
+# 各層で適度な広がりを持つように、強制的にアクティベーションの分布を調整しようとする処理である。
+# メリット：
+#  １．学習を早く進行させることができる。
+#　２．初期値にそれほど依存しない
+#　３．過学習を抑制する。(Dropoutなどの必要性を減らす)
+# 隠れ層のAffineレイヤと活性化関数レイヤの間に入れる。(最後の出力層前は不要)
+# 学習を行う際のミニバッチを単位として、ミニバッチごとに正規化を行う。
+class BatchNormalizationLayer:
+    """
+    http://arxiv.org/abs/1502.03167
+    """
+    def __init__(self, gamma, beta, momentum=0.9, running_mean=None, running_var=None):
+        self.gamma = gamma
+        self.beta = beta
+        self.momentum = momentum
+        self.input_shape = None # Conv層の場合は4次元、全結合層の場合は2次元  
+
+        # テスト時に使用する平均と分散
+        self.running_mean = running_mean
+        self.running_var = running_var  
+        
+        # backward時に使用する中間データ
+        self.batch_size = None
+        self.xc = None
+        self.std = None
+        self.dgamma = None
+        self.dbeta = None
+
+    # 訓練データかどうかで処理が変わる。
+    def forward(self, x, train_flg=True):
+        self.input_shape = x.shape
+        if x.ndim != 2:
+            N, C, H, W = x.shape
+            x = x.reshape(N, -1)
+
+        out = self.__forward(x, train_flg)
+        
+        return out.reshape(*self.input_shape)
+    
+    # TODO:計算式が全然わからず。。
+    def __forward(self, x, train_flg):
+        if self.running_mean is None:
+            N, D = x.shape
+            self.running_mean = np.zeros(D)
+            self.running_var = np.zeros(D)
+                        
+        if train_flg:
+            mu = x.mean(axis=0)
+            xc = x - mu
+            var = np.mean(xc**2, axis=0)
+            std = np.sqrt(var + 10e-7)
+            xn = xc / std
+            
+            self.batch_size = x.shape[0]
+            self.xc = xc
+            self.xn = xn
+            self.std = std
+            self.running_mean = self.momentum * self.running_mean + (1-self.momentum) * mu
+            self.running_var = self.momentum * self.running_var + (1-self.momentum) * var            
+        else:
+            # 6章(CNN前)ではこちらの処理を通るはず。
+            xc = x - self.running_mean
+            xn = xc / ((np.sqrt(self.running_var + 10e-7)))
+            
+        out = self.gamma * xn + self.beta 
+        return out
+
+    def backward(self, dout):
+        if dout.ndim != 2:
+            N, C, H, W = dout.shape
+            dout = dout.reshape(N, -1)
+
+        dx = self.__backward(dout)
+
+        dx = dx.reshape(*self.input_shape)
+        return dx
+
+    def __backward(self, dout):
+        dbeta = dout.sum(axis=0)
+        dgamma = np.sum(self.xn * dout, axis=0)
+        dxn = self.gamma * dout
+        dxc = dxn / self.std
+        dstd = -np.sum((dxn * self.xc) / (self.std * self.std), axis=0)
+        dvar = 0.5 * dstd / self.std
+        dxc += (2.0 / self.batch_size) * self.xc * dvar
+        dmu = np.sum(dxc, axis=0)
+        dx = dxc - dmu / self.batch_size
+        
+        self.dgamma = dgamma
+        self.dbeta = dbeta
+        
+        return dx
--- two_layer_net.py
+++ two_layer_net.py
@@ -10,7 +10,10 @@ import activation_func_layers
 
 class TwoLayerNet:
     def __init__(self,input_size,hidden_size,output_size,
-                 weight_init_std=0.01):
+                 weight_init_std=0.01,weight_decay_lambda=0.1):
+
+        # 引数受け取り
+        self.weight_decay_lambda = weight_decay_lambda
 
         # 重みの初期化(標準偏差使用)
         # 重みの初期化を適切な設定方法で行わないと、活性化関数の出力結果が偏ってしまい、
@@ -30,10 +33,17 @@ class TwoLayerNet:
         self.params['W1'] = np.random.randn(input_size,hidden_size) / np.sqrt(input_size) * np.sqrt(2)      # 平方根に渡すのはインプット側の層
         self.params['W2'] = np.random.randn(hidden_size,output_size) / np.sqrt(hidden_size) * np.sqrt(2)    # 平方根に渡すのはインプット側の層
 
+        # バッチ正規化のベータとガンマの値初期化
+        self.params['gamma'] = np.ones(hidden_size) # 隠れ層のサイズ分初期化
+        self.params['beta'] = np.zeros(hidden_size) # 隠れ層のサイズ分初期化
+
         # レイヤの生成
         self.layers = OrderedDict()
         self.layers['Affine1'] = activation_func_layers.AffineLayer(self.params['W1'],self.params['b1'])
+        self.layers['BatchNorm'] = activation_func_layers.BatchNormalizationLayer(self.params['gamma'], self.params['beta']) 
         self.layers['Relu1'] = activation_func_layers.ReluLayer()
+        # ドロップアウトはお試しでは入れても、意味がなかった。
+        #self.layers['Dropout'] = activation_func_layers.DropoutLayer()
         self.layers['Affine2'] = activation_func_layers.AffineLayer(self.params['W2'],self.params['b2'])
         self.lastLayer = activation_func_layers.SoftmaxWithLossLayer()
 
@@ -46,7 +56,21 @@ class TwoLayerNet:
     # 損失関数
     def loss(self,x,t):
         y = self.predict(x)
-        return self.lastLayer.forward(y,t)
+        # 重み減衰取得
+        weight_decay = self.__weight_decay()
+        return self.lastLayer.forward(y,t) + weight_decay
+
+    # L2正則化。重み減衰
+    # 過学習抑制のために使用する。そもそも過学習の原因は重みが大きい値をとることにより発生することが多々ある。
+    # ラムダはハイパーパラメータの一つだが、こちらを大きく設定すればするほど、大きな重みを取ることに対して強い減衰を加えることになる。
+    # 計算式：損失関数の出力結果 + 1/2 * λ(ラムダ) * Wの二乗
+    #        (すべての重みに対して実施する)
+    def __weight_decay(self):
+        weight_decay = 0
+        weight_decay += 0.5 * self.weight_decay_lambda * np.sum(self.params['W1'] ** 2)
+        weight_decay += 0.5 * self.weight_decay_lambda * np.sum(self.params['W2'] ** 2)
+        return weight_decay
+
 
     # 認識制度
     def accuracy(self,x,t):
